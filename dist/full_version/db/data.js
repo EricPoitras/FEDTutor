1a536d7c5e0e0983581c4051e1d799d05235aa49
Eric Poitras <EricPoitras@users.noreply.github.com>
2020-03-06 11:37:01 -0700

    Add files via upload

--- dist/full_version/db/data.js
+++ dist/full_version/db/data.js
@@ -6,7 +6,7 @@ var data = {
 		{
 			title: "Introduction to HTML",
 			content:
-				"<p>HTML stands for HyperText Markup Language:</p><ul><li>A markup language is a computer language that defines the structure and presentation of raw text.</li><li>In HTML, the computer can interpret raw text that is wrapped in HTML elements.</li><li>HyperText is text displayed on a computer or device that provides access to other text through links, also known as hyperlinks. You probably clicked on a couple of hyperlinks on your way to this module.</li></ul><p>Learning HTML is the first step in creating websites, but even a bit of knowledge can help you inject code snippets into newsletter, blog or website templates. As you continue learning, you can layer HTML with CSS and JavaScript to create visually compelling and dynamic websites. But for now, we’re going to focus on how to add and modify basic content on a page, like text, images, and videos.</p><p>In the code editor to the right, complete the <<span>h1</span>> element shown here following the proper syntax and type your name, then press Submit.</p>",
+				"<p>HTML stands for HyperText Markup Language:</p><ul><li>A markup language is a computer language that defines the structure and presentation of raw text.</li><li>In HTML, the computer can interpret raw text that is wrapped in HTML elements.</li><li>HyperText is text displayed on a computer or device that provides access to other text through links, also known as hyperlinks. You probably clicked on a couple of hyperlinks on your way to this module.</li></ul><p>Learning HTML is the first step in creating websites, but even a bit of knowledge can help you inject code snippets into newsletter, blog or website templates. As you continue learning, you can layer HTML with CSS and JavaScript to create visually compelling and dynamic websites. But for now, we’re going to focus on how to add and modify basic content on a page, like text, images, and videos.</p><p>In the code editor to the right, add an <<span>h1</span>> element following the proper syntax and type your name, then press Submit.</p>",
 			hint: [
 				{ id: "Hint #1", content: "<p>Try the following:</p><p><<span>element</span>>text content<<span>/element</span>></p>" },
 				{ id: "Solution #1", content: "<p>Try the following:</p><p><<span>h1</span>>John Doe<<span>/h1</span>></p>" }
@@ -22,7 +22,7 @@ var data = {
 		{
 			title: "HTML Syntax",
 			content:
-				"<p>A tag and the content between it is called an HTML element. It includes the following characteristics:</p><ul><li>HTML Tag — the element name, surrounded by an opening (<) and closing (>) angle bracket.</li><li>Opening Tag — the first HTML tag used to start an HTML element. The tag type is surrounded by opening and closing angle brackets.</li><li>Content — The information (text or other elements) contained between the opening and closing tags of an HTML element.</li><li>Closing tag — the second HTML tag used to end an HTML element. Closing tags have a forward slash (/) inside of them, directly after the left angle bracket.</li></ul><p>In the code editor to the right, complete the <<span>p</span>> element shown here following the proper syntax and type your current job title , then press Submit.</p>",
+				"<p>A tag and the content between it is called an HTML element. It includes the following characteristics:</p><ul><li>HTML Tag — the element name, surrounded by an opening (<) and closing (>) angle bracket.</li><li>Opening Tag — the first HTML tag used to start an HTML element. The tag type is surrounded by opening and closing angle brackets.</li><li>Content — The information (text or other elements) contained between the opening and closing tags of an HTML element.</li><li>Closing tag — the second HTML tag used to end an HTML element. Closing tags have a forward slash (/) inside of them, directly after the left angle bracket.</li></ul><p>In the code editor to the right, add a <<span>p</span>> element following the proper syntax and type your current job title , then press Submit.</p>",
 			hint: [
 				{ id: "Hint #1", content: "<p>Try the following:</p><p><<span>element</span>>text content<<span>/element</span>></p>" },
 				{ id: "Solution #1", content: "<p>Try the following:</p><p><<span>p</span>>Job Title<<span>/p</span>></p>" }
@@ -86,7 +86,7 @@ var data = {
 		{
 			title: "Headers",
 			content:
-				"<p>In HTML, there are six different headings, or heading elements. Headings can be used for a variety of purposes, like titling sections, articles, or other forms of content. The following is the list of heading elements available in HTML. They are ordered from largest to smallest in size. <ol><li><<span>h1></span> — used for main headings. All other smaller headings are used for subheadings.</li><li><<span>h2></span></li><li><<span>h3></span></li><li><<span>h4></span></li><li><<span>h5></span></li><li><<span>h6></span></li></ol></p><p>Complete a <<span>h6></span> element on a new line following the proper syntax and write your contact information.</p>",
+				"<p>In HTML, there are six different headings, or heading elements. Headings can be used for a variety of purposes, like titling sections, articles, or other forms of content. The following is the list of heading elements available in HTML. They are ordered from largest to smallest in size. <ol><li><<span>h1></span> — used for main headings. All other smaller headings are used for subheadings.</li><li><<span>h2></span></li><li><<span>h3></span></li><li><<span>h4></span></li><li><<span>h5></span></li><li><<span>h6></span></li></ol></p><p>Add an <<span>h6></span> element following the proper syntax and write your contact information.</p>",
 			hint: [
 				{ id: "Hint #1", content: "<p>Try the following:</p><p><<span>element</span>>text content<<span>/element</span>></p>" },
 				{ id: "Solution #1", content: "<p>Try the following:</p><p><<span>h6</span>>Contact Information<<span>/h6</span>></p>" }
@@ -102,7 +102,7 @@ var data = {
 		{
 			title: "Paragraphs",
 			content:
-				"<p>If you want to display text in HTML, you can use a paragraph. Paragraphs <<span>p></span> contain a block of plain text. Complete the <<span>p></span> element by following the proper syntax, and inside of the tags put the following text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus dictum enim a cursus hendrerit. Maecenas ac odio non turpis hendrerit tempor at nec nisl.'</<span>",
+				"<p>If you want to display text in HTML, you can use a paragraph. Paragraphs <<span>p></span> contain a block of plain text. Add a <<span>p></span> element by following the proper syntax, and inside of the tags put the following text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus dictum enim a cursus hendrerit. Maecenas ac odio non turpis hendrerit tempor at nec nisl.'</<span>",
 			hint: [
 				{ id: "Hint #1", content: "<p>Try the following:</p><p><<span>element</span>>text content<<span>/element</span>></p>" },
 				{ id: "Solution #1", content: "<p>Try the following:</p><p><<span>p</span>>Lorem ipsum ...<<span>/p</span>></p>" }
@@ -135,7 +135,7 @@ var data = {
 		{
 			title: "Div Containers",
 			content:
-				"<p>Div elements are very useful for grouping elements in your HTML together. These can contain any text or other HTML elements, such as links, images, or videos. Remember to always add two spaces of indentation when you nest elements inside of <<span>div>s</span> for better readability.</p><p>Add an opening <<span>div></span> tag before the paragraph element and a closing <span>/div></span> tag after the paragraph that features the 'Lorem ipsum' text following the proper syntax.</p>",
+				"<p>Div elements are very useful for grouping elements in your HTML together. These can contain any text or other HTML elements, such as links, images, or videos. Remember to always add two spaces of indentation when you nest elements inside of <<span>div>s</span> for better readability.</p><p>Add an opening <<span>div></span> tag before the paragraph element and a closing <<span>div></span> tag after the paragraph that features the 'Lorem ipsum' text following the proper syntax.</p>",
 			hint: [
 				{ id: "Hint #1", content: "<p>Try the following:</p><p><<span>element</span>>text content<<span>/element</span>></p>" },
 				{ id: "Solution #1", content: "<p>Try the following:</p><p><<span>div</span>>...<<span>/div</span>></p>" }
